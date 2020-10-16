6a3f130f2f5242dc549978dfe8aae7ba0f4a4d02
damokless <damokless@protonmail.com>
2020-10-16 11:17:54 +0200

    add exo1 & 2

---
+++ exo2.js
@@ -0,0 +1,17 @@
+function IsAPangrams(str = '') {
+    str = str.toLowerCase()
+    str = str.replace(/\s/g, '')
+    let myArray = str.split('')
+    let uniqueChars = [...new Set(myArray)]
+    console.log(uniqueChars)
+    console.log(uniqueChars.length)
+    if (uniqueChars.length == 26) {
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+
+console.log(IsAPangrams('The quick brown fox jumps over the lazy dog'))
