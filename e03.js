eace261bf1c93d921250c5b5c3db6f8b431c525b
damokless <damokless@protonmail.com>
2020-10-16 14:55:01 +0200

    push e03.js et ajout de e04-4.js

--- e03.js
+++ e03.js
@@ -1,5 +1,14 @@
 function marsos(str = '') {
-    
+    let erreur = 0
+
+    for(let i = 0; i < str.length; i++) {
+        const code = str.charCodeAt(i)
+
+        if ( code != 83 && code != 79) {
+            erreur++
+        }
+    }
+    return erreur
 }
 
 console.log(marsos('SOFSOSSISSOW'))
